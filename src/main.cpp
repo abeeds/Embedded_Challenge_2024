81c8d865ea1b57312c9892e1b8e2a1c877ef821c
Shubh Savani <sms9977@nyu.edu>
2024-05-01 10:27:31 -0400

    Created timer interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,48 @@ int n = windowSize * SR;
 double accelerometerData[windowSize * SR];
 
 // write function declarations here:
-void readAccelerometerData();
-float calculateNetAcceleration();
+float calculateNetAcceleration(float X, float Y, float Z);
+void setupTimer();
 
 float calculateNetAcceleration(float X, float Y, float Z) {
   return sqrt(X * X + Y * Y + Z * Z);
 }
 
+void setupTimer() {
+  // Setup TCCR0A register
+  TCCR0A = (1 << COM0A1) | (1 << WGM01);
+  // Clear OC0A on compare match -> COM0A1 = 1, COM0A0 = 0
+  // Disconnect OC0B - Normal Port operation -> COM0B1 = 0, COM0B0 = 0
+  // CTC mode with OCRA as TOP -> WGM02 = 0, WGM01 = 1, WGM00 = 0
+  
+  // Setup TCCR0B register
+  TCCR0B = 0;
+  // FOC0A = 0, FOC0B = 0
+  // WGM02 = 0
+
+  // Setup the OCR0A register
+  OCR0A = 249; // 50 ms
+
+  // Setup the TIMSK0 register
+  TIMSK0 = (1 << OCIE0A);
+  // Enable Compare Match A Interrupt -> OCIE0A = 1
+  
+}
+
+
 void setup() {
   Serial.begin(9600);
   CircuitPlayground.begin();
+
+  // Setup Timer 0 to generate an interrupt every 50 miliseconds
+  // This will be used to sample the accelerometer data
+  setupTimer();
 }
 
-void loop() {
 
+ISR(TIMER0_COMPA_vect) {
+  // This service routine will be called every 50 ms
+  // We will use this to sample the accelerometer data
   float X = CircuitPlayground.motionX();
   float Y = CircuitPlayground.motionY();
   float Z = CircuitPlayground.motionZ();
@@ -62,4 +90,8 @@ void loop() {
 
   //also isnt SR should be 1 / SR * 1000
   delay(SR);
+}
+
+void loop() {
+  //pass
 }
